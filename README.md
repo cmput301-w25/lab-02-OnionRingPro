7251528c25b1bb34b6781dff26d2a61a74594944
Chenqi Yang <onionring@Chenqis-MacBook-Air.local>
2025-01-15 00:27:00 -0700

    Complete all the requirements and the UI need to be further modified

--- README.md
+++ README.md
@@ -3,16 +3,16 @@
 
 ## Student Details
 
-- **Full Name:** `<Enter name>`
-- **CCID:** `<Enter ccid>`
+- **Full Name:** `Chenqi Yang`
+- **CCID:** `chenqi3`
 
 ## References and Resources
 
-List any resources used here, or simply put `N/A` if not applicable.
+Use chatGPT for the usage of Toast class.  
 
 ## Verbal Collaboration
 
 | Student Name | CCID      |
 | ------------ | --------- |
-| `student`    | `student` |
-| `<Add more>` | `<CCID>`  |
+| `N/A`    | `N/A` |
+| `N/A` | `N/A`  |
---
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView cityList;
+    ArrayList<String> dataList;
+    ArrayAdapter<String> cityAdapter;
+    Button addButton;
+    Button deleteButton;
+    Button inputButton;
+    EditText inputField;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        /* lab-02 */
+
+        cityList = findViewById(R.id.city_list);
+        dataList = new ArrayList<>();
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        addButton = findViewById(R.id.add_city_button);
+        deleteButton = findViewById(R.id.delete_city_button);
+        inputButton = findViewById(R.id.input_button);
+        inputField = findViewById(R.id.input_field);
+
+        addButton.setOnClickListener(v -> {
+            inputButton.setVisibility(View.VISIBLE);
+            inputField.setVisibility(View.VISIBLE);
+            inputButton.setTag("ADD");
+        });
+
+        deleteButton.setOnClickListener(v -> {
+            inputButton.setVisibility(View.VISIBLE);
+            inputField.setVisibility(View.VISIBLE);
+            inputButton.setTag("DELETE");
+        });
+
+        inputButton.setOnClickListener(v -> {
+            String inputText = inputField.getText().toString().trim();
+            if(inputText.isEmpty()){
+                Toast.makeText(MainActivity.this, "Please enter something", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (inputButton.getTag().equals("ADD")){
+                dataList.add(inputText);
+                Toast.makeText(MainActivity.this, "City Added", Toast.LENGTH_SHORT).show();
+            } else if (inputButton.getTag().equals("DELETE")){
+                if (dataList.contains(inputText)){
+                    dataList.remove(inputText);
+                    Toast.makeText(MainActivity.this, "City Deleted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "City Not Found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            /* update adapter and make input layout disappear */
+            cityAdapter.notifyDataSetChanged();
+            inputField.setText("");
+            inputField.setVisibility(View.GONE);
+            inputButton.setVisibility(View.GONE);
+        });
+
+
+
+    }
+}
